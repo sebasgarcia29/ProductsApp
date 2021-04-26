9dc2cf40cf96c81078e61c8a35c9b2632d7483d1
sebasgarcia29 <s.garcia@iimoney.co>
2021-04-26 14:47:58 -0500

    first commit

---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+export const LoginScreen = () => {
+  return (
+    <View>
+      <Text>LoginScreen</Text>
+    </View>
+  );
+};
---
+++ src/screens/ProtectedScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+export const ProtectedScreen = () => {
+  return (
+    <View>
+      <Text>ProtectedScreen</Text>
+    </View>
+  );
+};
